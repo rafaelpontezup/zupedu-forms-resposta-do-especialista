55e48469e32c5824ecef42792696f1d4522481b5
Rafael Ponte <rponte@gmail.com>
2021-07-23 12:15:23 -0300

    comentario de warning no script para evitar problemas com caractere de quebra de linha;

--- convert_md_to_docx.sh
+++ convert_md_to_docx.sh
@@ -1,12 +1,24 @@
+#!/bin/bash
+
 ##
 # Inspired by https://gist.github.com/hugorodgerbrown/5317616
 # Pandoc Docker Image: https://github.com/jagregory/pandoc-docker
 # Pandoc demos: https://pandoc.org/demos.html
 # Pandoc manual: https://pandoc.org/MANUAL.html
 #
+# WARNING: 
+# dont't allow git to replace LF by CRLF, otherwise the script will not run
+#
+# use this command to config your git:
+# git config core.autocrlf false
+#
+# more details: https://pt.stackoverflow.com/questions/44373/aviso-git-lf-will-be-replaced-by-crlf
+##
 
+# creates the docx folder
 mkdir docx 2> /dev/null
 
+# which files to convert?
 FILES=Formulario-*.md
 #FILES=Formulario-010-*.md
 
@@ -14,9 +26,11 @@ count=1
 for f in $FILES
 do
   # extension="${f##*.}"
-  filename="F5.$count.${f%.*}"
-  echo "[+] Converting '$f' to '$filename.docx'"
-  rm "$filename.docx" 2> /dev/null
-  docker run -v `pwd`:/source jagregory/pandoc -s --highlight-style tango -f markdown -t docx "$f" -o docx/"$filename.docx"
+  filename="F5.$count.${f%.*}.docx"
+
+  echo "[+] Converting '$f' to '$filename'"
+
+  rm "$filename" 2> /dev/null
+  docker run -v `pwd`:/source jagregory/pandoc -s --highlight-style tango -f markdown -t docx "$f" -o docx/"$filename"
   count=`expr $count + 1`
 done
